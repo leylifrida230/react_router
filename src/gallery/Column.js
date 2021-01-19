34efcaf5bbe62347ac1231791259d2bf1d13fe6e
leylifrida230 <55417475+leylifrida230@users.noreply.github.com>
2021-01-19 18:15:02 +0700

    gallery

--- src/gallery/Column.js
+++ src/gallery/Column.js
@@ -3,7 +3,7 @@ import ImgFrame from './ImgFrame'
 
 function Column({images}){
     return(
-        <div>
+        <div className="column">
             {images.map((img, i) => {
                 return<ImgFrame key={i} src={img.src} h={img.height} author={img.author} />
             })}
---
+++ src/gallery/data.js
@@ -0,0 +1,127 @@
+const getData = () => {
+    let data = [
+        {
+            src: 'https://i.pinimg.com/564x/e7/e7/d9/e7e7d970ff0a00e4715e721a13e99234.jpg',
+            height: '180',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/4b/ed/23/4bed2314b00a45972e9625290dc69464.jpg',
+            height: '420',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/21/4a/f8/214af86c1f70dc662b37336a5f2ca6f4.jpg',
+            height: '440',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/e1/e2/5e/e1e25e48bf70cedd819a7084333ef63d.jpg',
+            height: '470',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/91/b2/63/91b2637a447234f356d39d3d5b2a7c09.jpg',
+            height: '320',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/e5/62/07/e562073caa46db4c87f93d641c8d4913.jpg',
+            height: '430',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/564x/e7/e7/d9/e7e7d970ff0a00e4715e721a13e99234.jpg',
+            height: '180',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/4b/ed/23/4bed2314b00a45972e9625290dc69464.jpg',
+            height: '420',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/21/4a/f8/214af86c1f70dc662b37336a5f2ca6f4.jpg',
+            height: '440',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/e1/e2/5e/e1e25e48bf70cedd819a7084333ef63d.jpg',
+            height: '470',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/91/b2/63/91b2637a447234f356d39d3d5b2a7c09.jpg',
+            height: '320',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/e5/62/07/e562073caa46db4c87f93d641c8d4913.jpg',
+            height: '430',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/564x/e7/e7/d9/e7e7d970ff0a00e4715e721a13e99234.jpg',
+            height: '180',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/4b/ed/23/4bed2314b00a45972e9625290dc69464.jpg',
+            height: '420',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/21/4a/f8/214af86c1f70dc662b37336a5f2ca6f4.jpg',
+            height: '440',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/e1/e2/5e/e1e25e48bf70cedd819a7084333ef63d.jpg',
+            height: '470',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/91/b2/63/91b2637a447234f356d39d3d5b2a7c09.jpg',
+            height: '320',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/e5/62/07/e562073caa46db4c87f93d641c8d4913.jpg',
+            height: '430',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/564x/e7/e7/d9/e7e7d970ff0a00e4715e721a13e99234.jpg',
+            height: '180',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/4b/ed/23/4bed2314b00a45972e9625290dc69464.jpg',
+            height: '420',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/21/4a/f8/214af86c1f70dc662b37336a5f2ca6f4.jpg',
+            height: '440',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/e1/e2/5e/e1e25e48bf70cedd819a7084333ef63d.jpg',
+            height: '470',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/91/b2/63/91b2637a447234f356d39d3d5b2a7c09.jpg',
+            height: '320',
+            author: 'Awesome'
+        },
+        {
+            src: 'https://i.pinimg.com/236x/e5/62/07/e562073caa46db4c87f93d641c8d4913.jpg',
+            height: '430',
+            author: 'Awesome'
+        },
+    ]
+    return data;
+}
+
+export default getData;
